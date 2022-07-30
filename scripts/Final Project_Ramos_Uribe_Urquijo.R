a4a98f1765f0b0d174f6c9b2f08b4625bfc8e461
Paula Ramos <p.ramosj@uniandes.edu.co>
2022-07-29 23:03:51 -0500

    Var Otra infracción done

--- scripts/Final Project_Ramos_Uribe_Urquijo.R
+++ scripts/Final Project_Ramos_Uribe_Urquijo.R
@@ -1,5 +1,5 @@
 # Paula Ramos, Karen Uribe y Juan D. Urquijo
-# update: 28-07-2022
+# update: 29-07-2022
 ###----------------- Project Set 3----------###
 
 ##### ---Limpiar Ambiente --- ######
@@ -32,8 +32,8 @@ p_load(skimr, # summary data
 )
 
 
-
-setwd("C:/Users/kurib/OneDrive - Universidad de los Andes/Documentos/MECA/Github/Final-Project-Big-Data")
+setwd("C:/Users/pau_9/Documents/GitHub/Final-Project_Big-Data_Ramos-Uribe-Urquijo")
+#setwd("C:/Users/kurib/OneDrive - Universidad de los Andes/Documentos/MECA/Github/Final-Project-Big-Data")
 
 # 2017 
 base_2017_victimas <- read_excel("stores/Base_2017.xlsx", sheet = "VICTIMAS")
@@ -131,107 +131,142 @@ base_2018_victimas$EDAD_PROCESADA <- as.double(base_2018_victimas$EDAD_PROCESADA
 base_victimas <- bind_rows(base_2017_victimas, base_2018_victimas)
 
 
-
  ############### ========= CREACION VARIABLES ============ ##############
 
 #Validando la base, no podemos identificar el culpable. Propongo cambiar culpable por conductor. 
-#Incluir Número de vehículos involucrados, edad promedio conductor y edad promedio víctima y para el género contar el número de mujeres y hombres involucrados. 
+#Incluir N?mero de veh?culos involucrados, edad promedio conductor y edad promedio v?ctima y para el g?nero contar el n?mero de mujeres y hombres involucrados. 
 #Para fecha: as.Date(Base$variable, format = '%m/%d/%Y')
 
 
-#======= BASE 2017 ======#
-### Crear numero de vehículos involucrados por accidente
+#======= BASE ======#
+### Crear numero de veh?culos involucrados por accidente
 
-num_vehic <- base_2017_conductores %>% 
-  group_by(id)%>% 
+num_vehic <- base_conductores %>% 
+  group_by(idFormulario)%>% 
   summarise(Vehiculo = n())
 
-base_2017_siniestros <-base_2017_siniestros %>% left_join(num_vehic,by="id")
+base_siniestros <-base_siniestros %>% left_join(num_vehic,by="idFormulario")
 
-sum(is.na(base_2017_siniestros$Vehiculo))
+sum(is.na(base_siniestros$Vehiculo))
 
 ### Crear numero de victimas por accidente
-colnames(base_2017_victimas)
+colnames(base_victimas)
 
-num_vic <- base_2017_victimas %>% 
-  group_by(id)%>% 
+num_vic <- base_victimas %>% 
+  group_by(idFormulario)%>% 
   summarise(Numero = n())
 
-base_2017_siniestros <-base_2017_siniestros %>% left_join(num_vic,by="id")
-base_2017_siniestros$SD <- (base_2017_siniestros$GravedadNombre)
+base_siniestros <-base_siniestros %>% left_join(num_vic,by="idFormulario")
+base_siniestros$SD <- (base_siniestros$GravedadNombre)
 
-sum(is.na(base_2017_siniestros$Numero))
+sum(is.na(base_siniestros$Numero))
 
-#Asumir que no existen víctimas si existe NA
+#Asumir que no existen v?ctimas si existe NA
 
-base_2017_siniestros$Numero <- ifelse(is.na(base_2017_siniestros$Numero),
+base_siniestros$Numero <- ifelse(is.na(base_siniestros$Numero),
                                        yes = 0,
-                                       no = base_2017_siniestros$Numero)
+                                       no = base_siniestros$Numero)
+table(base_siniestros$Numero, base_siniestros$GravedadNombre)
+##Dado que existen accidentes con heridos y con muertos pero sin vÃ­ctimas, se asume que si tienen heridos o victimas, al menos tienen una vÃ­ctima
+base_siniestros$Numero <- ifelse((base_siniestros$GravedadNombre == "Con Heridos" | base_siniestros$GravedadNombre == "Con Muertos")& (base_siniestros$Numero == 0) ,
+                                 yes = 1,
+                                 no = base_siniestros$Numero)
 
-table(base_2017_siniestros$Numero, base_2017_siniestros$SD)
+table(base_siniestros$Numero, base_siniestros$GravedadNombre)
+
 
 ### Crear edad promedio conductores
 
-class(base_2017_conductores$EDAD_PROCESADA)
+class(base_conductores$EDAD_PROCESADA)
 
-m_edad_c <- base_2017_conductores %>% 
-  group_by(id)%>% 
+m_edad_c <- base_conductores %>% 
+  group_by(idFormulario)%>% 
   summarise(m_edad_c = round(mean(EDAD_PROCESADA), digits = 0))
 
-base_2017_siniestros <-base_2017_siniestros %>% left_join(m_edad_c,by="id")
+base_siniestros <-base_siniestros %>% left_join(m_edad_c,by="idFormulario")
+
+sum(is.na(base_siniestros$m_edad_c))
+
+base_siniestros = base_siniestros %>% group_by(idFormulario) %>% 
+  mutate(m_edad_c = ifelse(is.na(m_edad_c),
+                                 yes = median(base_conductores$EDAD_PROCESADA, na.rm = TRUE),
+                                 no = m_edad_c))
 
 ### Crear edad promedio victimas
 
-class(base_2017_victimas$EDAD_PROCESADA)
+class(base_victimas$EDAD_PROCESADA)
 
-m_edad_v <- base_2017_victimas %>% 
-  group_by(id)%>% 
+m_edad_v <- base_victimas %>% 
+  group_by(idFormulario)%>% 
   summarise(m_edad_v = round(mean(EDAD_PROCESADA), digits = 0))
 
-base_2017_siniestros <-base_2017_siniestros %>% left_join(m_edad_v,by="id")
-table(base_2017_siniestros$Numero, base_2017_siniestros$SD)
+base_siniestros <-base_siniestros %>% left_join(m_edad_v,by="idFormulario")
 
-### Variable género ********** OJO REVISAR
-# Hay dos opciones: contar el numero de mujeres y hombres o dummy mayoría mujeres =1 y 0 de lo contrario
+sum(is.na(base_siniestros$m_edad_v))
+
+base_siniestros = base_siniestros %>% group_by(idFormulario) %>% 
+  mutate(m_edad_v = ifelse(is.na(m_edad_v),
+                           yes = median(base_victimas$EDAD_PROCESADA, na.rm = TRUE),
+                           no = m_edad_v))
+
+### Variable g?nero ********** OJO REVISAR
+# Hay dos opciones: contar el numero de mujeres y hombres o dummy mayor?a mujeres =1 y 0 de lo contrario
 
 # == Numero de mujeres y hombres
 
 # Conductores
 
-num_hombres_c <- base_2017_conductores %>% 
-  group_by(id) %>% 
+num_hombres_c <- base_conductores %>% 
+  group_by(idFormulario) %>% 
   summarise(num_hombres_c = sum(Sexo == "MASCULINO"))
 
-num_mujeres_c <- base_2017_conductores %>% 
-  group_by(id) %>% 
+num_mujeres_c <- base_conductores %>% 
+  group_by(idFormulario) %>% 
   summarise(num_mujeres_c = sum(Sexo == "FEMENINO"))
 
-num_otro_c <- base_2017_conductores %>% 
-  group_by(id) %>% 
+num_otro_c <- base_conductores %>% 
+  group_by(idFormulario) %>% 
   summarise(num_otro_c = sum(Sexo == "NO APLICA"))
 
+sum(base_conductores$num_otro_c)
+base_siniestros <-base_siniestros %>% left_join(num_hombres_c,by="idFormulario")
+base_siniestros <-base_siniestros %>% left_join(num_mujeres_c,by="idFormulario")
+base_siniestros <-base_siniestros %>% left_join(num_otro_c,by="idFormulario")
 
-base_2017_siniestros <-base_2017_siniestros %>% left_join(num_hombres_c,by="id")
-base_2017_siniestros <-base_2017_siniestros %>% left_join(num_mujeres_c,by="id")
+table(base_siniestros$SD, base_siniestros$num_hombres_c)
+table(base_siniestros$SD, base_siniestros$num_mujeres_c)
+table(base_siniestros$SD, base_siniestros$num_otro_c)
 
-table(base_2017_siniestros$SD, base_2017_siniestros$num_hombres_c)
-table(base_2017_siniestros$SD, base_2017_siniestros$num_mujeres_c)
+sum(base_siniestros$num_otro_c, na.rm = TRUE)
 
 # Victimas
 
-num_hombres_v <- base_2017_victimas %>% 
-  group_by(id) %>% 
+num_hombres_v <- base_victimas %>% 
+  group_by(idFormulario) %>% 
   summarise(num_hombres_v = sum(Sexo == "MASCULINO"))
 
-num_mujeres_v <- base_2017_victimas %>% 
-  group_by(id) %>% 
+num_mujeres_v <- base_victimas %>% 
+  group_by(idFormulario) %>% 
   summarise(num_mujeres_v = sum(Sexo == "FEMENINO"))
 
-base_2017_siniestros <-base_2017_siniestros %>% left_join(num_hombres_v,by="id")
-base_2017_siniestros <-base_2017_siniestros %>% left_join(num_mujeres_v,by="id")
+base_siniestros <-base_siniestros %>% left_join(num_hombres_v,by="idFormulario")
+base_siniestros <-base_siniestros %>% left_join(num_mujeres_v,by="idFormulario")
 
+table(base_siniestros$num_hombres_v, base_siniestros$GravedadNombre )
+table(base_siniestros$num_mujeres_v,base_siniestros$GravedadNombre )
+
+base_siniestros = base_siniestros %>% group_by(idFormulario) %>% 
+  mutate(num_hombres_v = ifelse(is.na(num_hombres_v),
+                           yes = 0,
+                           no = num_hombres_v))
+
+base_siniestros = base_siniestros %>% group_by(idFormulario) %>% 
+  mutate(num_mujeres_v = ifelse(is.na(num_mujeres_v),
+                                yes = 0,
+                                no = num_mujeres_v))
 
 ############### ========= ORGANIZACION VARIABLES RELEVANTES ============ ##############
+
 
 ## Variables relevantes: id, Dia, Fecha, GravedadCod, GravedadNombre, ClaseCodigo, ClaseNombre, Latitud, Longitud, Direccion,
                       #  Localidad, HoraOcurrencia, TipoTiempo, Vehiculo, Numero, m_edad_c, m_edad_v, num_hombres_c, num_mujeres_c, 
@@ -239,74 +274,81 @@ base_2017_siniestros <-base_2017_siniestros %>% left_join(num_mujeres_v,by="id")
 
 ### DEPENDENT VARIABLE
 
-# Variable Dependiente: Grado de severidad del siniestro (1 con muertos, 2 con heridos, 3 con daños) - Dejarla en 1 severo 0 leve
+# Variable Dependiente: Grado de severidad del siniestro (1 con muertos, 2 con heridos, 3 con da?os) - Dejarla en 1 severo 0 leve
+glimpse(base_siniestros)
 
 
-class(base_2017_siniestros$GravedadNombre) #Preferiria dejar solo dos categorias
+class(base_siniestros$GravedadNombre) #Preferiria dejar solo dos categorias
 
-base_2017_siniestros$GravedadNombre[base_2017_siniestros$GravedadNombre== "Con Heridos"] <- "Con Muertos" 
+base_siniestros$GravedadNombre[base_siniestros$GravedadNombre== "Con Heridos"] <- "Con Muertos" 
 
-base_2017_siniestros$GravedadNombre <- factor(base_2017_siniestros$GravedadNombre, 
-                                                 levels = c("Con Muertos", "Solo Daños"),
+base_siniestros$GravedadNombre <- factor(base_siniestros$GravedadNombre, 
+                                                 levels = c("Con Muertos", "Solo DaÃ±os"),
                                                  labels = c("Severo", "Leve"))  ## Poner variable como categorica
-class(base_2017_siniestros$GravedadNombre)
-levels(base_2017_siniestros$GravedadNombre)
-table(base_2017_siniestros$GravedadNombre)
+class(base_siniestros$GravedadNombre)
+levels(base_siniestros$GravedadNombre)
+table(base_siniestros$GravedadNombre)
 
 ### HUMAN FACTOR
 
 # Accident Type: ClaseNombre
 
-class(base_2017_siniestros$ClaseNombre)
-table(base_2017_siniestros$ClaseNombre)
+class(base_siniestros$ClaseNombre)
+table(base_siniestros$ClaseNombre)
 
-base_2017_siniestros$TipoAccidente <- factor(base_2017_siniestros$ClaseNombre, 
+base_siniestros$TipoAccidente <- factor(base_siniestros$ClaseNombre, 
                                            levels = c("Atropello", "Autolesion", "Caida Ocupante", "Choque",
                                                       "Incendio", "Otro", "Volcamiento"),
                                            labels = c("Atropello", "Autolesion", "Caida Ocupante", "Choque",
                                                       "Incendio", "Otro", "Volcamiento"))  ## Poner variable como categorica
-class(base_2017_siniestros$TipoAccidente)
-table(base_2017_siniestros$TipoAccidente)
+class(base_siniestros$TipoAccidente)
+table(base_siniestros$TipoAccidente)
 
 # Violation of law: 
 
 #1. Embriaguez
 
-class(base_2017_siniestros$CON_EMBRIAGUEZ)
-table(base_2017_siniestros$CON_EMBRIAGUEZ)
+class(base_siniestros$CON_EMBRIAGUEZ)
+table(base_siniestros$CON_EMBRIAGUEZ)
 
-base_2017_siniestros$CON_EMBRIAGUEZ <- factor(base_2017_siniestros$CON_EMBRIAGUEZ, 
+base_siniestros$CON_EMBRIAGUEZ <- factor(base_siniestros$CON_EMBRIAGUEZ, 
                                               levels = c("SI", "NO"),
                                               labels = c("SI", "NO"))  ## Poner variable como categorica
 
-
-
-class(base_2017_siniestros$CON_EMBRIAGUEZ)
-table(base_2017_siniestros$CON_EMBRIAGUEZ)
+class(base_siniestros$CON_EMBRIAGUEZ)
+table(base_siniestros$CON_EMBRIAGUEZ)
 
 #2. Velocidad
 
-class(base_2017_siniestros$CON_VELOCIDAD)
-table(base_2017_siniestros$CON_VELOCIDAD)
+class(base_siniestros$CON_VELOCIDAD)
+table(base_siniestros$CON_VELOCIDAD)
 
-base_2017_siniestros$CON_VELOCIDAD <- factor(base_2017_siniestros$CON_VELOCIDAD, 
+base_siniestros$CON_VELOCIDAD <- factor(base_siniestros$CON_VELOCIDAD, 
                                               levels = c("SI", "NO"),
                                               labels = c("SI", "NO"))  ## Poner variable como categorica
 
 
 
-class(base_2017_siniestros$CON_VELOCIDAD)
-table(base_2017_siniestros$CON_VELOCIDAD)
+class(base_siniestros$CON_VELOCIDAD)
+table(base_siniestros$CON_VELOCIDAD)
 
 
 #3. Otro: No Porta Licencia, No lleva cinturon, No lleva caso, No lleva Chaleco
 
+##Dado que existen otro tipo de "infracciones", se crea una categorÃ­a adicional
 
+base_conductores$OtraInfrac<-ifelse((base_conductores$PortaLicencia == "N" | base_conductores$LLevaCinturon == "N" |
+                                        base_conductores$LLevaCasco == "N" | base_conductores$LLevaChaleco == "N"), 1, 0)
+OtraInfrac<-base_conductores %>% group_by(idFormulario) %>% summarise(OtraInfrac = sum(OtraInfrac))
 
+base_victimas$OtraInfraV <-ifelse((base_victimas$LLevaCinturon == "N" | base_victimas$LLevaCasco == "N" |
+                                   base_victimas$LlevaChaleco == "N"),1, 0)
+OtraInfraV<-base_victimas %>% group_by(idFormulario) %>% summarise(OtraInfrav = sum (OtraInfraV))
 
-
-
-
+OI <-left_join(OtraInfrac,OtraInfraV, by="idFormulario")
+OI$Otrainfra_c_v<-ifelse((OI$OtraInfrac>=1 | OI$OtraInfrav>=1 ),1, 0)
+OI <- OI%>% select (1,4) 
+base_siniestros <-left_join(base_siniestros,OI, by="idFormulario")
 
 
 # Gender ****** PENDIENTE
